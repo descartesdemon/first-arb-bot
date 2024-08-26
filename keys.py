d8f3ac08630edb67311e4551420f4d3bf959cb0c
Kevin Tseng <143230611+descartesdemon@users.noreply.github.com>
2024-08-26 11:41:28 -0700

    added example keys.py

---
+++ keys.py
@@ -0,0 +1,19 @@
+#EXAMPLE STRUCTURE OF keys.py
+keyDict = {
+    'coinbase': {
+        'apiKey': '...',
+        'secret': '...',
+    },
+}
+
+keyDict_sandbox = {
+    'binance': {
+        'apiKey': '...',
+        'secret': '...',
+    },
+    'coinbaseexchange': {
+        'apiKey': '...',
+        'secret': '...',
+        'password': '...',
+    }
+}
